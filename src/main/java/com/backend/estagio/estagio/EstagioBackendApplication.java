7a640eb0a94b8093338780e9ed120b8a762b36ea
Eric Silva Mendes <ericsilvaccp@gmail.com>
2020-11-23 22:02:41 -0300

    first commit

---
+++ src/main/java/com/backend/estagio/estagio/EstagioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.estagio.estagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstagioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/estagio/estagio/EstagioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.estagio.estagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstagioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
